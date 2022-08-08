c46f93703d09c69e6349c3645520c7ac90a8d71c
Hamza-Nasir <hamzanasir123123@gmail.com>
2022-08-08 18:10:56 +0500

    Added Icon to weather and changed /about

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 const weatherForm = document.querySelector('form');
 const msg1 = document.getElementById('message-1');
 const msg2 = document.getElementById('message-2');
+const weather_icon = document.getElementById('weather-icon');
 
 msg1.textContent = 'From JavaScript';
 
@@ -28,6 +29,7 @@ weatherForm.addEventListener('submit', (e) => {
         else {
             msg1.textContent = `${data.address}, ${data.country}`;
             msg2.textContent = `${data.weather_desc}, ${data.data.temperature} C`;
+            weather_icon.src = data.data.weather_icons[0];
 
             console.log(data);
         }
